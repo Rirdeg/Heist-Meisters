5a2042c7a30b7ccf01dd07fe1f9c5652344656dd
Rirdeg <52657039+Rirdeg@users.noreply.github.com>
2019-11-14 11:48:48 +0000

    Update Door.gd
    comment

--- Scripts/Door.gd
+++ Scripts/Door.gd
@@ -2,6 +2,15 @@ extends Area2D
 
 var is_in_player_range = false
 
+<<<<<<< Updated upstream
+=======
+# ------------------------------------------------- #
+# Would like to make door stay upon when character is infront of it
+# idea to emove animation player and lerp() the position of the sprite through code
+# This would fix guards getting stuck on doors or doors not opening when they stay within the bounds
+#-------------------------------------------------- #
+
+>>>>>>> Stashed changes
 func _on_Door_body_entered(body):
 	if not body == Global.Player and not $AnimationPlayer.is_playing():
 		open()
